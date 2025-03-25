262781c9a16b4ecf5e3d3dde564f7a21f1d28034
mykola-ovchynnik <ovchynnik.mykola@gmail.com>
2025-03-26 00:35:51 +0100

    add files with test data, update file extension to .dat

--- src/config/constants.py
+++ src/config/constants.py
@@ -3,5 +3,5 @@ import os
 DATA_DIR = "data"
 os.makedirs(DATA_DIR, exist_ok=True)
 
-CONTACTS_FILE = os.path.join(DATA_DIR, "contacts.pkl")
-NOTES_FILE = os.path.join(DATA_DIR, "notes.pkl")
+CONTACTS_FILE = os.path.join(DATA_DIR, "contacts.dat")
+NOTES_FILE = os.path.join(DATA_DIR, "notes.dat")
