c61dcf25eba88fc29622474d5e27007b0816f083
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2025-03-25 20:36:42 +0000

    Notes classes were initialised

---
+++ src/models/book/book.py
@@ -0,0 +1,9 @@
+from src.models.notes.note_book import NoteBook
+
+class Book:
+    def __init__(self):
+        self.notes = NoteBook()
+        # self.contacts = AddressBook()
+
+    def __str__(self):
+        return f"Notes: {self.notes}"
---
+++ src/models/notes/note_book.py
@@ -0,0 +1,18 @@
+from collections import UserList
+
+from src.utils.find_elements import find_element
+
+class NoteBook(UserList):
+    def add_note(self, note):
+        element = find_element(self.data, lambda x: x.name == note.name)
+        if not element:
+            self.data.appent(note)
+        else:
+            raise KeyError("Note with such name already exists.")
+
+    def delete_note(self, name):
+        element = find_element(self.data, lambda x: x.name == name)
+        if element:
+            self.data.pop(element)
+        else:
+            raise KeyError("Note with such name was not found.")
