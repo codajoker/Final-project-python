371d6212c4ef82994370c7a3e11fbd7af4cf6544
codajoker <84983434+codajoker@users.noreply.github.com>
2025-03-25 13:33:30 +0100

    add: add Field and Record Class

---
+++ src/services/contact_service.py
@@ -0,0 +1,14 @@
+from collections import UserDict
+from datetime import date, datetime
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data[name] if name in self.data.keys() else None
+    
+    def delete(self, name):
+        self.data.pop(name)
+
+ 
