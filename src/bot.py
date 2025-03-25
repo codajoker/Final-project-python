341ee3ab963c2ec5518d0e75ce71d56dc3cbe7e2
mykola-ovchynnik <ovchynnik.mykola@gmail.com>
2025-03-25 09:53:14 +0100

    refactor: reorganize project structure and add .gitignore

---
+++ src/bot.py
@@ -0,0 +1,43 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+
+def main():
+    # contacts = AddressBook()
+
+    print("Welcome to the assistant bot!")
+
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            pass
+            # print(add_contact(args, contacts))
+        elif command == "change":
+            pass
+            # print(change_contact(args, contacts))
+        elif command == "phone":
+            pass
+            # print(get_phone(args, contacts))
+        elif command == "add-birthday":
+            pass
+            # print(add_birthday(args, contacts))
+        elif command == "show-birthday":
+            pass
+            # print(show_birthday(args, contacts))
+        elif command == "birthdays":
+            pass
+            # print(birthdays(contacts))
+        elif command == "all":
+            pass
+            # print(contacts)
+        else:
+            print("Invalid command.")
